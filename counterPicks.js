f071e86f45c8a246912f3a64126f7e67c8ac8abe
= <osorno.gabo@gmail.com>
2021-04-27 00:25:30 -0500

    Fixes

--- counterPicks.js
+++ counterPicks.js
@@ -2,7 +2,7 @@ const counterSearchInput = document.getElementById("counterSearchInput");
 const toggleBar = document.getElementsByClassName("toggleBar")[0];
 const toggleArrows = document.getElementsByClassName("toggleArrows")[0];
 const championsToggle = document.getElementsByClassName("selectionBox")[0];
-let toggleDisplayed = false;
+let toggleDisplayed = true;
 const selectionBox = document.getElementsByClassName("selectionBox")[0];
 let champNames = [];
 
@@ -20,7 +20,7 @@ toggleArrows.addEventListener("click", () => {
 const getChampsInfo = (async() => {
     const champsInfoResponse = await fetch('http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json');
     const champsInfo = await champsInfoResponse.json();
-     champNames = Object.keys(champsInfo.data);
+    champNames = Object.keys(champsInfo.data);
 
     createSelectionRows(champNames)
 })()
@@ -48,29 +48,35 @@ const createSelectionRows = (names) => {
 
             selectionRow = document.createElement("div");
             selectionRow.className = "selectionRow";
-            console.log("hey")
-
         }
+
         selectionRow.appendChild(champButtons[i]);
     }
 }
 
 counterSearchInput.addEventListener("keyup", (evnt) => {
-    let filteredNames = champNames.filter((name) => {
-        for (let i = 0; i < name.length; i++){
-            if (evnt.target.value[0].toLowerCase() === name[i].toLowerCase()){
-                for (let j = 1; j < evnt.target.value.length; j++){
-                    if (name[i+j] !== evnt.target.value[j]) {
-                        return false;
-                    }
-                }
-                return true;
-            }
+    if (evnt.target.value === "") {
+        while (selectionBox.firstChild) {
+            selectionBox.removeChild(selectionBox.lastChild);
         }
-    })
+        createSelectionRows(champNames);
+    } else {
+        let filteredNames = champNames.filter((name) => {
+            for (let i = 0; i < name.length; i++){
+                if (evnt.target.value[0].toLowerCase() === name[i].toLowerCase()) {
+                    for (let j = 1; j < evnt.target.value.length; j++){
+                        if (name[i+j] !== evnt.target.value[j]) {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+            }
+        })
 
-    while (selectionBox.firstChild) {
-        selectionBox.removeChild(selectionBox.lastChild);
-      }
-    createSelectionRows(filteredNames);
+        while (selectionBox.firstChild) {
+            selectionBox.removeChild(selectionBox.lastChild);
+        }
+        createSelectionRows(filteredNames);
+    }
 })
